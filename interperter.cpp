1d8aaaa6da471e28d1beb293dc55605aa89dcd18
SteevoMcCool <96311500+SteevoMcCool@users.noreply.github.com>
2024-01-05 22:59:03 -0500

    Update interperter.cpp

--- interperter.cpp
+++ interperter.cpp
@@ -5,7 +5,7 @@
 #include <cassert>
 using namespace std;
 
-vector<string> types = ["string","number","vpn","bool"]
+vector<string> types = ["string","number","vpn","bool","scope"];
 
 bool isAinTypes( string A){
     for (i==0; i <types.size(), i=i+1){
@@ -61,7 +61,7 @@ class Scope{
                 string eq = getToken();
                 if (eq == "="){
                     string restOfLine = evalUntil(";");
-                    void* value = inlineEval(restOfLine);
+                    Value value = inlineEval(restOfLine);
                     if (!typeCheck(value,rtype)){
                         error("Cannot convert types!")
                         return false
@@ -70,21 +70,33 @@ class Scope{
                     var.name = name;
                     var.value = value;
                     var.rtype = rtype;
-                    variables.push(var);
+                    variables.push_back(var);
                 }else if (eq=="("){
                     Scope scope;
                     while (true){
-                        name = getToken();
+                        string name1 = getToken();
                         if (name == ")") {
                             break;
                         }
                         Variable v1;
-                        v1.name = name;
+                        v1.name = name1;
                         if isAinTypes(v1.name){
                             v1.rtype = v1.name;
                             v1.name = getToken();
                         };
+                        scope.variables.push_back(v1);
+                        string comma = getToken();
+                        if (comma!=","){
+                            error("Expected comma!");
+                        };
                     }
+                    string eq = getToken();
+                    scope.raw = fromOpenertoCloser("{","}")
+                    Value value;
+                    value.store = scope;
+                    value.type = "scope"
+                    var.value = value
+                    variables.push_back(var)
                 }else {
                     error("Invalid declaration!")
                     return false
